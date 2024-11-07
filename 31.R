d03a3f8dfded45c8f70e5c09fe59e91b0fade57c
SpencerGoldberg14 <smgoldbe@hamilton.edu>
2024-11-07 16:44:17 +0000

    class 11/7 changes

--- 31.R
+++ 31.R
@@ -1,4 +1,5 @@
 # install.packages(c("lubridate", "dplyr", "ggplot2"))
+# start of class 1
 library(dplyr)
 library(ggplot2)
 library(olsrr)
@@ -74,8 +75,92 @@ predict.lm(mod.full, data.frame(airTemp=20,log.age=log(2),
                                 log.precip=6, BorealV=0),
            interval="confidence")
 
+# start of class 2
+ETdat <- read.csv("/cloud/project/ETdata.csv")
+library(lubridate)
+library(ggplot2)
+library(forecast)
+library(dplyr)
 
+# average fields for each month for almonds
+almond <- ETdat %>% 
+  filter(crop == "Almonds") %>% 
+  group_by(date) %>% 
+  summarise(ET.in = mean(Ensemble.ET, na.rm=TRUE))
+# visualize the data
+ggplot(almond, aes(x=ymd(date),y=ET.in))+
+  geom_point()+
+  geom_line()+
+  labs(x="year", y="Monthy evapotranspiration (in)")
 
+# almond ET time series
+almond_ts <- ts(almond$ET.in, 
+                start = c(2016,1), 
+                frequency= 12) 
 
+# decompose almond ET time series
+almond_dec <- decompose(almond_ts)
+# plot decomposition
+plot(almond_dec)
 
+acf(na.omit(almond_ts), # remove missing data
+    lag.max = 24) # look at 2 years (24 months)
+
+almond_y <- na.omit(almond_ts)
+model1 <- arima(almond_y , 
+                order = c(1,0,0)) 
+model1
+
+model4 <- arima(almond_y , # data 
+                order = c(4,0,0)) # first number is AR order all other numbers get a 0 to keep AR format
+model4
+
+# calculate fit
+AR_fit1 <- almond_y - residuals(model1) 
+AR_fit4 <- almond_y - residuals(model4)
+#plot data
+plot(almond_y)
+# plot fit
+points(AR_fit1, type = "l", col = "tomato3", lty = 2, lwd=2)
+points(AR_fit4, type = "l", col = "darkgoldenrod4", lty = 2, lwd=2)
+legend("topleft", c("data","AR1","AR4"),
+       lty=c(1,2,2), lwd=c(1,2,2), 
+       col=c("black", "tomato3","darkgoldenrod4"),
+       bty="n")
+
+newAlmond <- forecast(model4)
+newAlmond
+
+#make dataframe for plotting
+newAlmondF <- data.frame(newAlmond)
+
+# set up dates
+years <- c(rep(2021,4),rep(2022,12), rep(2023,8))
+month <- c(seq(9,12),seq(1,12), seq(1,8))
+newAlmondF$dateF <- ymd(paste(years,"/",month,"/",1))
+
+# make a plot with data and predictions including a prediction interval
+ggplot() +
+  geom_line(data = almond, aes(x = ymd(date), y = ET.in))+
+  xlim(ymd(almond$date[1]),newAlmondF$dateF[24])+  
+  geom_line(data = newAlmondF, aes(x = dateF, y = Point.Forecast),
+            col="red") +  
+  geom_ribbon(data=newAlmondF, 
+              aes(x=dateF,ymin=Lo.95,
+                  ymax=Hi.95), fill=rgb(0.5,0.5,0.5,0.5))+ 
+  theme_classic()+
+  labs(x="year", y="Evapotranspiration (in)")
+
+# start of HW
+# recommended CO2 transformation
+ghg <- read.csv("/cloud/project/Deemer_GHG_Data.csv")
+ghg$co2 <- 1/(ghg$co2+1000)
+ghg$co2
+
+# multiple regression
+# creates a model object
+mod.full <- lm(log.ch4 ~ airTemp+
+                 log.age+mean.depth+
+                 log.DIP+
+                 log.precip+ BorealV, data=ghg) 
 
